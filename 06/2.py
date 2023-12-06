df2c949027c027eee97fab86f1153d542ed9d356
Clemens Siebler <clemens.siebler@gmail.com>
2023-12-06 10:54:01 +0100

    Added more efficient solution

--- 06/2.py
+++ 06/2.py
@@ -1,6 +1,5 @@
 import os
 import re
-import numpy as np
 
 # load data.txt from current folder
 with open(os.path.join(os.path.dirname(__file__), 'data.txt'), 'r') as f:
@@ -11,12 +10,20 @@ data = data.split('\n')
 time = int(data[0].split(':')[1].replace(' ', ''))
 distance = int(data[1].split(':')[1].replace(' ', ''))
 
-times_won = 0
+# To win, we need to solve distance < t_charge * (time - t_charge)
+sol1 = int(time/2 + ((-1 * time/2)**2 - distance) ** 0.5)
+sol2 = int(time/2 - ((-1 * time/2)**2 - distance) ** 0.5)
 
-for t in range(0, time + 1):
-    if (distance < t * (time - t)):
-        times_won += 1
-    if (t % 1000000 == 0):
-        print(f"Checked {t}/{time} options")
+# Winning count is the difference
+print(f"Winning count is {abs(sol1 - sol2)}")
 
-print(f"Won race {times_won} times")
+# Brute force solution
+
+# times_won = 0
+# for t in range(0, time + 1):
+#     if (distance < t * (time - t)):
+#         times_won += 1
+#     if (t % 1000000 == 0):
+#         print(f"Checked {t}/{time} options")
+
+# print(f"Won race {times_won} times")
